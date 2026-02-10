bcc45ca660f9851822424d5b17a73cbe15cb345f
Richardczl98 <Richardchen@radixark.ai>
2026-02-11 01:51:52 +0800

    Add Step3-VL-10B doc

--- sidebars.js
+++ sidebars.js
@@ -173,6 +173,11 @@ const sidebars = {
               id: 'autoregressive/StepFun/Step3.5',
               label: 'Step3.5 [NEW]',
             },
+            {
+              type: 'doc',
+              id: 'autoregressive/StepFun/Step3-VL-10B',
+              label: 'Step3-VL-10B [NEW]',
+            },
           ],
         }
       ],
